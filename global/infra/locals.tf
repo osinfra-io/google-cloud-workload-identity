fd913af7bf3271769c7b1981cf2379bb2cb8cc3a
Brett Curtis <brett@osinfra.io>
2023-09-02 11:53:42 -0400

    Move assertion ref to attribute mapping

--- global/infra/locals.tf
+++ global/infra/locals.tf
@@ -5,10 +5,11 @@ locals {
 
   workload_identity = {
     "github-actions" = {
-      attribute_condition = var.environment == "sb" ? null : "assertion.ref==\"refs/heads/main\""
+      # attribute_condition = var.environment == "sb" ? null : "assertion.ref==\"refs/heads/main\""
       attribute_mapping = {
         "attribute.actor"      = "assertion.actor"
         "attribute.aud"        = "assertion.aud"
+        "attribute.ref"        = "assertion.ref"
         "attribute.repository" = "assertion.repository"
         "google.subject"       = "assertion.sub"
       }
