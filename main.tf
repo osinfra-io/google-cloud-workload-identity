6a19a60f9438214276454ddf5827608be174be89
Brett Curtis <brett@osinfra.io>
2024-08-03 08:26:41 -0400

    Update Terraform child module paths (#181)

--- main.tf
+++ main.tf
@@ -30,20 +30,21 @@ provider "datadog" {
 # https://github.com/osinfra-io/terraform-datadog-google-integration
 
 module "datadog" {
-  source = "github.com/osinfra-io/terraform-datadog-google-integration//global?ref=v0.2.0"
+  source = "github.com/osinfra-io/terraform-datadog-google-integration?ref=v0.2.1"
   count  = var.enable_datadog ? 1 : 0
 
-  api_key         = var.datadog_api_key
-  is_cspm_enabled = true
-  labels          = local.labels
-  project         = module.project.project_id
+  api_key                            = var.datadog_api_key
+  is_cspm_enabled                    = true
+  is_security_command_center_enabled = true
+  labels                             = local.labels
+  project                            = module.project.project_id
 }
 
 # Google Project Module (osinfra.io)
 # https://github.com/osinfra-io/terraform-google-project
 
 module "project" {
-  source = "github.com/osinfra-io/terraform-google-project//global?ref=v0.3.0"
+  source = "github.com/osinfra-io/terraform-google-project?ref=v0.3.0"
 
   billing_account                 = var.billing_account
   cis_2_2_logging_sink_project_id = var.cis_2_2_logging_sink_project_id
